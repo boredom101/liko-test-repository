d19dac2c1ffc58b4ecca3bc0d80ca52de790ad2a
Yisroel Newmark <ynewmark@new-host-2.home>
2018-05-22 20:53:43 -0400

    Share package

---
+++ share_0.0.0/init.lua
@@ -0,0 +1,15 @@
+local path, trigger = (...)
+local triggerData = {select(3,...)}
+
+local term = require("terminal")
+
+local PATH = term.getPATH()
+local PKGProgramsPath = path.."Programs/;"
+
+if trigger == "enable" then
+  PATH = PATH..PKGProgramsPath
+  term.setPATH(PATH)
+elseif trigger == "disable" then
+  PATH = PATH:gsub(PKGProgramsPath,"")
+  term.setPATH(PATH)
+end
